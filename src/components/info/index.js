1d79131a62d44e55b37b65aff2c2e2eb9e17f0a9
filadam <filip.adamczyk90@gmail.com>
2019-08-24 15:56:04 +0200

    style fixes

--- src/components/info/index.js
+++ src/components/info/index.js
@@ -4,22 +4,24 @@ import paint from './img/paint.png'
 
 function Info() {
     return (
-        <div>
-            <div className="container jobs__section--wrapper">
+        <div style={{
+            backgroundColor: "rgb(245, 245, 245)"
+        }}>
+            <div className="container info__section--wrapper">
                 <div className="row">
-                    <div className="col-l jobs__container">
-                        <div className="jobs__item">
-                            <div className="item__content">
+                    <div className="col-l info__container">
+                        <div className="info__item">
+                            <div className="infoItem item__content">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam urna sapien, ultrices et tincidunt a, euismod quis lacus. Nam luctus ullamcorper consectetur. Proin iaculis semper eros in varius. Proin id enim turpis. Integer tempor, arcu sit amet auctor posuere, tortor urna dictum dui, id ultrices sapien arcu in enim. Suspendisse sit amet magna nulla. Fusce libero diam, lobortis quis ipsum quis, aliquam mollis orci. Praesent in leo volutpat, semper justo vel, ornare arcu.
+                                    <b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit. Aliquam urna sapien, ultrices et tincidunt a, euismod quis lacus. Nam luctus ullamcorper consectetur. Proin iaculis semper eros in varius. Proin id enim turpis. Integer tempor, arcu sit amet auctor posuere, tortor urna dictum dui, id ultrices sapien arcu in enim. Suspendisse sit amet magna nulla. Fusce libero diam, lobortis quis ipsum quis, aliquam mollis orci. Praesent in leo volutpat, semper justo vel, ornare arcu.
                                     Donec condimentum lectus eu turpis ornare, eu porttitor libero varius. Etiam bibendum ac nibh vel pharetra. Aliquam mauris ex, gravida ac orci eu, posuere vehicula ex. Nulla rutrum est at ex sagittis, et elementum est semper. Vestibulum laoreet sapien ipsum, vel sodales erat gravida nec. Nulla facilisi. Suspendisse tempor dictum lacus vitae eleifend. Nam fermentum egestas faucibus. Nam sed luctus diam.
                                     Nam id eros ultrices, iaculis nisi id, facilisis ex. Donec at tellus tellus. Integer dignissim, justo sed vestibulum rutrum, libero justo euismod ex, et placerat felis dui dapibus erat. In ut dignissim mi. Aliquam ut odio vitae metus mattis faucibus.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm jobs__bigimg">
-                        <img src={paint} />
+                    <div className="col-sm info__bigimg">
+                        <img src="https://geronigo.com/blog/wp-content/uploads/2019/03/picture3-10-900x450.jpg" />
                     </div>
 
                 </div>
--- src/components/layout/navbar/topbar/index.js
+++ src/components/layout/navbar/topbar/index.js
@@ -3,33 +3,55 @@ import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhoneVolume, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import $ from 'jquery'
 
 const iconStyles = {
     color: "#da2128"
 }
 
 class TopBar extends React.Component {
+
     render() {
+
         return (
-            <div className="topBar">
-                
-                    <div className="container topBar--contents">
-                        <p>
-                            <FontAwesomeIcon style={iconStyles} className="faIcon" icon={faPhoneVolume} />
-                            123 456 789
+            <div
+                className="topBar"
+                onMouseOver={() => {
+                    $(".faIcon").addClass("animeTopBar")
+                }}
+                onMouseLeave={() => {
+                    $(".animeTopBar").removeClass("animeTopBar")
+                }}
+            >
+                <div className="container topBar--contents">
+                    <p>
+                        <FontAwesomeIcon
+                            style={iconStyles}
+                            className="faIcon"
+                            icon={faPhoneVolume}
+                        />
+                        123 456 789
                         </p>
-                        <p>
-                            <FontAwesomeIcon style={iconStyles} className="faIcon" icon={faEnvelope} />
-                            sklep@assassinscreed.pl
+                    <p>
+                        <FontAwesomeIcon
+                            style={iconStyles}
+                            className="faIcon"
+                            icon={faEnvelope}
+                        />
+                        sklep@assassinscreed.pl
                         </p>
-                        <p>
-                            <FontAwesomeIcon style={iconStyles} className="faIcon" icon={faFacebookF} />
-                            asassinar
+                    <p>
+                        <FontAwesomeIcon
+                            style={iconStyles}
+                            className="faIcon"
+                            icon={faFacebookF}
+                        />
+                        asassinar
                         </p>
 
-                    </div>
-                    
                 </div>
+
+            </div>
         )
     }
 }
