575e1425b479f3b443122dd4d3523dc070524aa9
filadam <filip.adamczyk90@gmail.com>
2019-08-24 16:32:16 +0200

    style fixes

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -6,12 +6,12 @@ function Articles() {
         <div className="container aboutme__section--wrapper">
             <div className="row">
                 <div className="col-sm aboutme__bigimg">
-                    <img src="https://picsum.photos/600/450?grayscale" />
+                    <img src="http://www.kajtur.pl/wp-content/uploads/2014/01/image_d-234.jpg" />
                 </div>
                 <div className="col-l aboutme__container">
                     <div className="aboutme__item">
                         <div className="item__thumb">
-                            <img src="https://picsum.photos/200/200?grayscale" />
+                            <img src="http://www.weglomix.pl/bmz_cache/c/Odkryty%20Paintball%20Airsoft%20Polowanie%20Netto%20Okulary%20Gogle%20Taktyczne%20Ochrona%20Oczu%20Okulary%20Okulary%20Turystyka%20Okulary%20Sportowe%20Siatki%20Metalowej%20ali-46917714_4_200x200.jpg" />
                         </div>
                         <div className="item__content">
                             <p>
@@ -23,7 +23,7 @@ function Articles() {
                     </div>
                     <div className="aboutme__item">
                         <div className="item__thumb">
-                            <img src="https://picsum.photos/200/200?grayscale" />
+                            <img src="https://ae01.alicdn.com/kf/HTB1N12SX4_rK1RkHFqDq6yJAFXac/PCP-Paintball-Airsoft-Tank-High-Pressure-Carbon-Fiber-Cylinders-M18-Bottle-0-35L-0-5L-4500psi.jpg_200x200.jpg" />
                         </div>
                         <div className="item__content">
                             <p>
--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -9,91 +9,94 @@ const iconStyles = {
     marginRight: 30
 }
 
-function Footer() {
-    return (
-        <div className="footerBg">
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <div className="footerBg">
 
 
-            <div
-                className="footer__container"
-            >
                 <div
-                    className="container"
-                    style={{
-                        backgroundImage: { footerBg }
-                    }}
+                    className="footer__container"
                 >
-                    <div className="row">
-                        <div className="col-sm">
-                            <div className="content__container">
-                            <p
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                    }}
-                                >
-                                    <p>INFORMACJE</p>
-                                    <a href="#">Informacje o sklepie</a>
-                                    <a href="#">Wysyłka</a>
-                                    <a href="#">Regulamin</a>
-                                    <a href="#">Polityka prywatności</a>
-                                    <a href="#">Odstąpienie od umowy</a>
-                                </p>
+                    <div
+                        className="container"
+                        style={{
+                            backgroundImage: { footerBg }
+                        }}
+                    >
+                        <div className="row">
+                            <div className="col-sm">
+                                <div className="content__container">
+                                    <p
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                        }}
+                                    >
+                                        <p>INFORMACJE</p>
+                                        <a href="#">Informacje o sklepie</a>
+                                        <a href="#">Wysyłka</a>
+                                        <a href="#">Regulamin</a>
+                                        <a href="#">Polityka prywatności</a>
+                                        <a href="#">Odstąpienie od umowy</a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="content__container">
-                                <p
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                    }}
-                                >
-                                    <p>MOJE KONTO</p>
-                                    <a href="#">Zarejestruj się</a>
-                                    <a href="#">Moje zamówienia</a>
-                                    <a href="#">Koszyk</a>
-                                    <a href="#">Ulubione</a>
-                                    <a href="#">Historia Transakcji</a>
-                                </p>
+                            <div className="col-sm">
+                                <div className="content__container">
+                                    <p
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                        }}
+                                    >
+                                        <p>MOJE KONTO</p>
+                                        <a href="#">Zarejestruj się</a>
+                                        <a href="#">Moje zamówienia</a>
+                                        <a href="#">Koszyk</a>
+                                        <a href="#">Ulubione</a>
+                                        <a href="#">Historia Transakcji</a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="content__container">
-                                <p>
-                                    <p>KONTAKT</p>
-                                    <p style={{ fontSize: "22px", }}>+48 574 046 421</p>
-                                    <p style={{ fontSize: "22px", marginTop: "-15px", }}>kontakt@assassinarmy.pl</p>
-                                    <FontAwesomeIcon style={iconStyles} className="faIcon fa-2x" icon={faFacebookF} />
-                                    <FontAwesomeIcon style={iconStyles} className="faIcon fa-2x" icon={faInstagram} />
-                                </p>
+                            <div className="col-sm">
+                                <div className="content__container">
+                                    <p>
+                                        <p>KONTAKT</p>
+                                        <p style={{ fontSize: "22px", }}>+48 574 046 421</p>
+                                        <p style={{ fontSize: "22px", marginTop: "-15px", }}>kontakt@assassinarmy.pl</p>
+                                        <FontAwesomeIcon style={iconStyles} className="faIcon fa-2x" icon={faFacebookF} />
+                                        <FontAwesomeIcon style={iconStyles} className="faIcon fa-2x" icon={faInstagram} />
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-sm">
-                            <div className="content__container">
-                                <p
-                                    style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                    }}
-                                >
-                                    <p>MOJE ZAMÓWIENIA</p>
-                                    <a href="#">Sprawdź status zamówienia</a>
-                                    <a href="#">Śledź przesyłkę</a>
-                                    <a href="#">Reklamacje</a>
-                                    <a href="#">Zwroty</a>
-                                    <a href="#">Informacje o wymianie</a>
-                                </p>
+                            <div className="col-sm">
+                                <div className="content__container">
+                                    <p
+                                        style={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                        }}
+                                    >
+                                        <p>MOJE ZAMÓWIENIA</p>
+                                        <a href="#">Sprawdź status zamówienia</a>
+                                        <a href="#">Śledź przesyłkę</a>
+                                        <a href="#">Reklamacje</a>
+                                        <a href="#">Zwroty</a>
+                                        <a href="#">Informacje o wymianie</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="footerl d-block d-sm-block d-xl-none">
+                <div className="footerl d-block d-sm-block d-xl-none">
 
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 
