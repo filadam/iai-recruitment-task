e657d7b0909404011dfe85b9e812296581e067d8
filadam <filip.adamczyk90@gmail.com>
2019-09-09 16:39:29 +0200

    build package

---
+++ build/precache-manifest.13fd154b2f99fdf2a9a376f661c9f3ed.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0805188fe32a6f9601ad7f2cc5b9fefa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41829f4db52e700fd1d7",
+    "url": "/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "5599c468f221a9fdf5b0",
+    "url": "/static/css/main.4c8cfdf0.chunk.css"
+  },
+  {
+    "revision": "41829f4db52e700fd1d7",
+    "url": "/static/js/2.62d2695a.chunk.js"
+  },
+  {
+    "revision": "5599c468f221a9fdf5b0",
+    "url": "/static/js/main.e3e80549.chunk.js"
+  },
+  {
+    "revision": "e7f4a6a759c7748921f1",
+    "url": "/static/js/runtime~main.def81781.js"
+  },
+  {
+    "revision": "28351a6e4e58ced993cba52353659e8e",
+    "url": "/static/media/1.28351a6e.jpg"
+  },
+  {
+    "revision": "be62b720b2b9af93348eb89feba18985",
+    "url": "/static/media/11.be62b720.jpg"
+  },
+  {
+    "revision": "ee742187ca366b9cd86492f19cbab899",
+    "url": "/static/media/2.ee742187.jpg"
+  },
+  {
+    "revision": "8ea944f5bfd21186574c5bf98692e43a",
+    "url": "/static/media/25.8ea944f5.jpg"
+  },
+  {
+    "revision": "b88a95361e5f97fbff6fc65ec589bd8b",
+    "url": "/static/media/3.b88a9536.jpg"
+  },
+  {
+    "revision": "7358fa49374f13638c38c3181ff0a220",
+    "url": "/static/media/badge.7358fa49.png"
+  },
+  {
+    "revision": "389669d9f8ba1e334463aff2a4f1a63b",
+    "url": "/static/media/bg.389669d9.png"
+  },
+  {
+    "revision": "d61019bc2cc75edc4e948b92289ac811",
+    "url": "/static/media/footer.d61019bc.png"
+  },
+  {
+    "revision": "896164b271cbc1f9478ba311e9639d02",
+    "url": "/static/media/pb1000x300.896164b2.jpg"
+  },
+  {
+    "revision": "744bd82e18d21e6daa73b23ee5297445",
+    "url": "/static/media/pb1000x500.744bd82e.jpg"
+  },
+  {
+    "revision": "2e926251d6c91e1717a760aa9a16105e",
+    "url": "/static/media/pb400x700.2e926251.jpg"
+  }
+]);
