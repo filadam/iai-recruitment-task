30bdcc8a9992e8789701c92584ad07874571f612
filadam <filip.adamczyk90@gmail.com>
2019-09-14 12:54:50 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import NavBar from './components/layout/navbar/index'
 import CarouselA from './components/carousell/index'
 import Cards from './components/cards/index'
 import Info from './components/info/index'
 import Articles from './components/articles/index'
-import Footer from './components/layout/footer/index'
 
 function App() {
   return (
     <>
-      <NavBar />
+
+
       <CarouselA />
       <Cards />
       <Articles />
       <Info />
-      <Footer />
+
+
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import ProductPage from './components/products/productpage';
+import Footer from './components/layout/footer/index'
+import NavBar from './components/layout/navbar/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <>
+        <NavBar />
+        <Router>
+
+            <Route exact path='/' component={App} />
+            <Route exact path='/products/testproduct' component={ProductPage} />
+
+        </Router >
+        <Footer />
+    </>
+
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
